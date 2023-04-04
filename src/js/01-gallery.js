fa6883ee0a0ca06c5a643174f5c132b636b42e86
MateuszKar2 <system505@protonmail.com>
2023-04-04 21:21:42 +0200

    changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,17 @@
+//---------------
+//przed rozpoczęciem projektu 
+//utworzyłem repozytorium
+//sklonowałem projekt z parcel-project-template 
+//przerzuciłem to wszystko do mojego projektu
+//zamieniłem pliki startowe w src z tym co tam było. 
+//otworzyłem swój projekt w vsc 
+//w terminalu wpisałem nmp install i npm start.
+//---------------
+//dodaję bibliotekę SimpeLightbox
+
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
