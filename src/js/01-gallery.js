51beec0d7634e45628fa18fec3395a0e7eb75dac
MateuszKar2 <system505@protonmail.com>
2023-04-05 12:29:52 +0200

    add first gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,10 +7,23 @@
 //otworzyłem swój projekt w vsc 
 //w terminalu wpisałem nmp install i npm start.
 //---------------
-//dodaję bibliotekę SimpeLightbox
+//aby umieścić kod CSS biblioteki w projekcie dodaje:
+//import(opisany w dokumentacji) tzn.import SimpleLightbox...
+//dodatkowy import stylów tzn."simpleLightbox/dist/..."
 //importuję galerię obazów z gallery- items
+//wywołuję galerię obrazów
 //pobieram gallery- za pomocą querySelector
-//
+//przypisuje zmiennej "markup"- createIte... o parametrze(galerię obrazów)
+//za pomocą metody .insert...(beforeend) dadaję "gallery" wewnątrz elementu po wszystkich dzieciach
+//tworzę funkcję createIte...(której parametrem będzie(item)- obraz)
+//za pomocą operatora return zwracam wartość z ciała funkcji do "galleryItems", gdzie zostanie wywołana
+//wykorzystuję metodę .map, celem otrzymania przekształconej kopii tablicy
+//Parametrami metody map. są: "preview"-mały obraz, "original"-duży obraz, "description"-opis obrazka.
+//implementuje gotowy szablon tagów HTML/CSS wprowadzając zmianne 
+//łączę elementy tablicy za pomocą metody .join
+//KOrzystam z bibliotekę simplelightbox aby dodać podpis do obrazków
+//wykorzystuję opcje "caption"- podpis/ "cationsData" - pobrać podpis z otrzymanego atrybutu/
+//i "captionDalay - dodaje opóżnienie w wyświetleniu podpisu"
 
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
@@ -23,7 +36,7 @@ console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
  const markup = createItemsMarkup(galleryItems);
  gallery.insertAdjacentHTML("beforeend", markup);
-
+ 
 
 
  function createItemsMarkup(item) {
@@ -36,7 +49,7 @@ const gallery = document.querySelector(".gallery");
  src="${preview}"
  alt="${description}"/></a></div>`;})
      .join("");
-  
+
  }
 
 
