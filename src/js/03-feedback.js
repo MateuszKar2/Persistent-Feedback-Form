d386db42a7429022c188450629a55b054edff1b3
MateuszKar2 <system505@protonmail.com>
2023-04-05 13:46:00 +0200

    add third gallery

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+ //zapoznaję się z dokumentacją biblioteki odtwarzacza Vimeo
+//zmienną player nawiązuje do sekcji pre-existing player w bibliotece
+//deklaruję zmienną playrtCurrentTime, któa odpowiada za aktualny czas video
+//do zmiennej iframe pobieram id video
+//zmiennej pauseTime przekazuje jej event w f.strzałkowej 
+//dla magazynu lokalnego(localStorage) używam metody .setItem za pomocą której mogę dodać nowy wpis jako parę klucz:wartość
+//przy pomocy dokumentacji metody .on rozpoczynam śledźenie "timpeupdate" - aktualizacji czasu odtwarzania
+//przy przeładowaniu strony użyto metody .SetCurrentTime wzorując się na bibliotece
+//do magazyna lokalnego(localStorage) użyłem metody .getItem która zwraca wartość (klucza)
+//wykorzystałem operator logiczny(or) tj. ||, jeżeli jeden operand jest true zwraca true
 
+
+
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const player = new Player(iframe);
+const playerCurrentTime = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+
+
+const pauseTime = e => {
+  localStorage.setItem(playerCurrentTime, e.seconds);
+};
+
+player.on('timeupdate', throttle(pauseTime, 1000));
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)) || 0)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
