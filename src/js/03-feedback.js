02abda6dc4657551ff4e61f30e728810efb8d02a
MateuszKar2 <system505@protonmail.com>
2023-04-05 21:31:00 +0200

    updates add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,13 @@
 
-//instaluje biblioteki 'lodash.throttle' 
- 
-
+//instaluje bibliotekę 'lodash.throttle' 
+//deklaruje mienną "LOCALSTORAGE_KEY" gdzie będe zapisywał obiekt z polami email i mesage
+//deklaruje zmienną "form" do której pobieram za pomocą klasy <form> z HTML
+//dodaje dektor zdarzeń <addEventListner> na input i submit
+//używam techniki <throttle> która kontroluje ile razy funkcja może być wywołana w określonym czasie
+//przypisuje zmiennej let wartość JSON.sparse- czyli konwertuje na wiersz
+//do magazyna lokalnego(localStorage) użyłem metody .getItem która zwraca wartość (klucza)
+//wykorzystałem operator logiczny(or) tj. ||, jeżeli jeden operand jest true zwraca true
+//
 
 import throttle from 'lodash.throttle';
 
