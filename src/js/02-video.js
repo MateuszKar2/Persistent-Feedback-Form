1ad03a62f58fa53f460fa6821ca92fddc081df7f
MateuszKar2 <system505@protonmail.com>
2023-04-05 12:50:45 +0200

    add secund gallery

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+ //zapoznaję się z dokumentacją biblioteki odtwarzacza Vimeo
+ //
+ //
  
+ 
+ 
+ import throttle from 'lodash.throttle';
+ import Player from '@vimeo/player';
+ 
+ const playerCurrentTime = 'videoplayer-current-time';
+ const iframe = document.querySelector('#vimeo-player');
+ const player = new Player(iframe);
+ 
+ const pauseTime = e => {
+   localStorage.setItem(playerCurrentTime, e.seconds);
+ };
+ 
+ player.on('timeupdate', throttle(pauseTime, 1000));
+ 
+ player
+   .setCurrentTime(JSON.parse(localStorage.getItem(playerCurrentTime)) || 0)
+   .then(function (seconds) {})
+   .catch(function (error) {
+     switch (error.name) {
+       case 'RangeError':
+         break;
+ 
+       default:
+         break;
+     }
+   });
