572cce090ebf15a8f69e6d4a533a60da8b884f25
MateuszKar2 <system505@protonmail.com>
2023-05-24 21:56:15 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,9 @@
-//---------------
-//przed rozpoczęciem projektu 
-//utworzyłem repozytorium
-//sklonowałem projekt z parcel-project-template 
-//przerzuciłem to wszystko do mojego projektu
-//zamieniłem pliki startowe w src z tym co tam było. 
-//otworzyłem swój projekt w vsc 
-//w terminalu wpisałem nmp install i npm start.
-//---------------
-//aby umieścić kod CSS biblioteki w projekcie dodaje:
-//import(opisany w dokumentacji) tzn.import SimpleLightbox...
-//dodatkowy import stylów tzn."simpleLightbox/dist/..."
-//importuję galerię obazów z gallery- items
-//wywołuję galerię obrazów
+
+//importuję galerię obazów z gallery-items
 //pobieram gallery- za pomocą querySelector
-//przypisuje zmiennej "markup"- createIte... o parametrze(galerię obrazów)
-//za pomocą metody .insert...(beforeend) dadaję "gallery" wewnątrz elementu po wszystkich dzieciach
-//tworzę funkcję createIte...(której parametrem będzie(item)- obraz)
+//przypisuje zmiennej "markup"- createItemsMarkup o parametrze(galerię obrazów)
+//za pomocą metody .insertAdjacentHTML(beforeend) dadaję "gallery" wewnątrz elementu po wszystkich dzieciach
+//tworzę funkcję createItemsMarkup(której parametrem będzie(item)- obraz)
 //za pomocą operatora return zwracam wartość z ciała funkcji do "galleryItems", gdzie zostanie wywołana
 //wykorzystuję metodę .map, celem otrzymania przekształconej kopii tablicy
 //Parametrami metody map. są: "preview"-mały obraz, "original"-duży obraz, "description"-opis obrazka.
@@ -25,18 +13,15 @@
 //wykorzystuję opcje "caption"- podpis/ "cationsData" - pobrać podpis z otrzymanego atrybutu/
 //i "captionDalay - dodaje opóżnienie w wyświetleniu podpisu"
 
-// Add imports above this line
 import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css"
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
- const markup = createItemsMarkup(galleryItems);
- gallery.insertAdjacentHTML("beforeend", markup);
+const markup = createItemsMarkup(galleryItems);
  
+
+gallery.insertAdjacentHTML("beforeend", markup);
 
 
  function createItemsMarkup(item) {
@@ -49,14 +34,11 @@ const gallery = document.querySelector(".gallery");
  src="${preview}"
  alt="${description}"/></a></div>`;})
      .join("");
-
+ 
  }
-
 
 const lightbox = new SimpleLightbox('.gallery a', {
   caption: true,
   captionsData: 'alt',
   captionDelay: 50,
 });
-
-   
